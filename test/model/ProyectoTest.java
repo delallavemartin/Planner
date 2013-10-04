7ac23fe7dc37ab5deca0bd514f9c1f030a7dd8c0
delallavemartin <delalallave.martin@gmail.com>
2013-10-03 21:37:33 -0300

    - Agregue el test para soyPersonaClave de la clase Proyecto.

--- test/model/ProyectoTest.java
+++ test/model/ProyectoTest.java
@@ -2,30 +2,43 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import model.evento.Tarea;
+import model.recurso.EmpleadoImp;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class ProyectoTest {
 	
 	private Proyecto proyecto = new Proyecto();
+	@Mock
+	private EmpleadoImp empleado;
+	@Mock
+	private Set<EmpleadoImp> empleadosClaves;
+	@Mock
 	private Tarea tarea1;
+	@Mock
 	private Tarea tarea2;
+	@Mock
 	private Tarea tarea3;
+	@Mock
 	private Tarea tarea4;
 
 	@Before
 	public void setUp() throws Exception {
-		tarea1 = Mockito.mock(Tarea.class);
-		tarea2 = Mockito.mock(Tarea.class);
-		tarea3 = Mockito.mock(Tarea.class);
-		tarea4 = Mockito.mock(Tarea.class);
-		proyecto.agregarTarea(tarea1);
-		proyecto.agregarTarea(tarea2);
-		proyecto.agregarTarea(tarea3);
-		proyecto.agregarTarea(tarea4);
+		MockitoAnnotations.initMocks(this);
+		this.proyecto.agregarTarea(tarea1);
+		this.proyecto.agregarTarea(tarea2);
+		this.proyecto.agregarTarea(tarea3);
+		this.proyecto.agregarTarea(tarea4);
+		this.empleadosClaves.add(empleado);
+		this.proyecto.setEmpleadosClaves(empleadosClaves);
 	}
 
 	@Test
@@ -34,7 +47,12 @@ public class ProyectoTest {
 		Mockito.doReturn(true).when(tarea2).estoyCompletada();
 		Mockito.doReturn(true).when(tarea3).estoyCompletada();
 		Mockito.doReturn(false).when(tarea4).estoyCompletada();
-		assertTrue(proyecto.porcentajeDeTareasCompletadas()==75);
+		assertTrue(this.proyecto.porcentajeDeTareasCompletadas()==75);
 	}
 	
+	@Test
+	public void testSoyPersonaClave() {
+		Mockito.when(empleadosClaves.contains(empleado)).thenReturn(true);
+		assertTrue(this.proyecto.soyPersonaClave(empleado));
+	}
 }
